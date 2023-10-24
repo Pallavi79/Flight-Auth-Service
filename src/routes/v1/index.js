70366374e2a8260e37fef736dac0da6dbf41b8ce
Pallavi <pallavi3749@gmail.com>
2023-10-24 12:46:24 +0530

    signup user

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const userRouter = require('./user-routes');
 
 const {InfoController} = require('../../controllers');
 
 router.get('/info',InfoController.info);
+
+router.use('/user', userRouter);
 
 module.exports = router;
