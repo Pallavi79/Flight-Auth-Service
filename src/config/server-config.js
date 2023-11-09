5ea99dc3a6803d588748cfa8e67684763ce8b142
Pallavi <pallavi3749@gmail.com>
2023-11-09 12:40:32 +0530

    added limiter and proxy

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,4 +6,6 @@ module.exports={
     SALT_ROUNDS: process.env.SALT_ROUNDS,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY,
+    SEARCH_SERVICE:process.env.SEARCH_SERVICE,
+    BOOKING_SERVICE:process.env.BOOKING_SERVICE
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const {ServerConfig, Logger} = require('./config');
 const apiRoutes = require('./routes');
-
+const {createProxyMiddleware}=require('http-proxy-middleware');
+const rateLimit =require('express-rate-limit');
 const app = express();
+
+const limiter =  rateLimit({
+    windowMs:2*60*1000,
+    max:3
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(limiter)
 
+app.use('/searchService', createProxyMiddleware({
+    target:ServerConfig.SEARCH_SERVICE,changeOrigin:true, 
+    pathRewrite:{'/searchService':'/'}
+}));
+
+app.use('/bookingService', createProxyMiddleware({
+    target:ServerConfig.BOOKING_SERVICE,changeOrigin:true,
+    pathRewrite:{'/bookingService':'/'}
+}));
 
 app.use('/api',apiRoutes);
 
